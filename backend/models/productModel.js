2efde590b37a34afd38297583ebee8d3516b29fb
simron6917 <simron.shrestha@featherwebs.com>
2021-06-26 12:36:20 +0545

    final ivyearth

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,72 +1,96 @@
 import mongoose from 'mongoose'
 
-const reviewSchema= mongoose.Schema({
-    name:{
-        type:String,
-        required: true
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-    rating:{
-        type:Number,
-        required: true
-    },
-    comment:{
-        type:String,
-        required: true
-    }
-}, {
+  },
+  {
     timestamps: true,
-})
-const productSchema = mongoose.Schema({
-   user:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-   },
-    name:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true,
-    },
-    brand:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true,
-    },
-    description:{
-        type:String,
-        required:true,
-    },
-    reviews: [reviewSchema],
-    rating:{
-        type:Number,
-        required:true,
-        default:0
-    },
-    numReviews:{
-        type:Number,
-        required:true,
-        default:0
-    },
-    price:{
-        type:Number,
-        required:true,
-        default:0
-    },
-    countInStock:{
-        type:Number,
-        required:true,
-        default:0
-    },
-}, {
-    timestamps: true
-})
+  }
+)
 
-const Product= mongoose.model ('Product', productSchema)
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name:{
+      type:String,
+      required:true
+  },
+  genus:{
+      type:String,
+      required:true
+  },
+  type:{
+      type:String,
+      required:true
+  },
+  light:{
+      type:String,
+      required:true
+  },
+  description:{
+      type:String,
+      required:true,
+  },
+  image:{
+      type:String,
+      required:true,
+  },
+  propagation:{
+      type:String,
+      required:true,
+  },
+  height:{
+      type:String,
+      required:true
+  },
+  width:{
+      type:String,
+      required:true,
+  },
+  flower_color:{
+      type:String,
+      required:true,
+  },
+  foliage_color:{
+      type:String,
+      required:true,
+  },
+  problem_solvers:{
+      type:String,
+      required:true,
+  },
+  special_features:{
+      type:String,
+      required:true,
+  },
+  care_must_knows:{
+      type:String,
+      required:true,
+  },
+  reviews: [reviewSchema],
+   rating: {
+       type: Number,
+       required: true,
+       default: 0,
+     },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
 
 export default Product
--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Spinner} from 'react-bootstrap'
+import { Spinner } from 'react-bootstrap'
 
-const Loader= () => {
-    return (
-        <Spinner animaion='border' role ='status' style ={{ width: '100px', height:'100px', margin :'Auto', display:'block'}}>
-            <span className='sr-only'> Loading... </span>
-        </Spinner>
-    )
+const Loader = () => {
+  return (
+    <Spinner
+      animation='border'
+      role='status'
+      style={{
+        width: '100px',
+        height: '100px',
+        margin: 'auto',
+        display: 'block',
+      }}
+    >
+      <span className='sr-only'>Loading...</span>
+    </Spinner>
+  )
 }
 
 export default Loader
